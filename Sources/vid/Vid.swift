a3421dda19fce81382922b0f9100317c39cd26d7
Mark Malstrom <mark@malstrom.me>
2026-07-17 15:47:01 -0500

    ci: publish cross-platform release binaries

--- Sources/vid/Vid.swift
+++ Sources/vid/Vid.swift
@@ -5,6 +5,7 @@ struct Vid: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "vid",
         abstract: "Convert and organize media with FFmpeg.",
+        version: "0.1.0",
         subcommands: [
             RemuxCommand.self,
             TagCommand.self,
